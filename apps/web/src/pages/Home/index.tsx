b307a852c202a83cc29146c501ff23463a45802b
Faris <mfarismalik@gmail.com>
2022-10-21 10:00:35 +0800

    updated to paywong@service staging account

--- apps/web/src/pages/Home/index.tsx
+++ apps/web/src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ const Home = () => {
   useEffect(() => {
     const paywong = new Paywong();
     paywong.initialize(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGFpbXMiOnsieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJhcHAiXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYXBwIiwieC1oYXN1cmEtYXBwLWlkIjoiZDgzMTNkZGEtODZkZS00YmFlLWIyODAtNGE0MzM0NDQ1MzRiIiwieC1oYXN1cmEtYWNjb3VudC1pZCI6IlR1TnBiMjBXTSIsIngtaGFzdXJhLWFwcC1zYWx0IjoiWUoyIn0sImlhdCI6MTY2NDg2NjIxNn0.MW7T3DTXz3OU3AzLNK42vsSWcc2rQQglaQ-1rGuyhCg',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGFpbXMiOnsieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJhcHAiXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYXBwIiwieC1oYXN1cmEtYXBwLWlkIjoiMjcxYzQyM2ItNjBmYS00ZmUxLWI1NGEtNTE2OGM2NzMwODUxIiwieC1oYXN1cmEtYWNjb3VudC1pZCI6IlhLWUQ1U0lIWiIsIngtaGFzdXJhLWFwcC1zYWx0IjoiUWIyIn0sImlhdCI6MTY2NjMxNzU0Mn0.d0D6297dJ2q34ve4gCs6qEvHQDUpgvUGJ9JYW-Wn-do',
       'staging'
     );
   }, [tabIndex]);
