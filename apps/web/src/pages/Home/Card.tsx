5f40ffbd4001d9b407e32de6fdbe1d2ef3ecf08b
Faris <mfarismalik@gmail.com>
2023-01-25 10:28:41 +0800

    switch to npm regirstry

--- apps/web/src/pages/Home/Card.tsx
+++ apps/web/src/pages/Home/Card.tsx
@@ -1,6 +1,6 @@
 import {Flex, Text} from '@chakra-ui/react';
 import {TextStyle} from 'theme/types';
-import {PaywongButton, PaywongEnvironment} from '@paywong/react';
+import {PaywongButton, PaywongEnvironment} from 'paywong-react';
 
 interface ICard {
   type: string;
